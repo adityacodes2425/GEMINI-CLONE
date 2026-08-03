a0b2b52d22ee981df246db48c3f6103c246872fd
Aditya <adityasaxena91316@gmail.com>
2026-08-03 18:22:30 +0530

    Biuld sidebar components

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import {
+  MdMenu,
+  MdAdd,
+  MdHelpOutline,
+  MdHistory,
+  MdSettings,
+} from "react-icons/md";
+import "./Sidebar.css";
+
 function Sidebar() {
+  const [extended, setExtended] = useState(true);
+
   return (
     <div className="sidebar">
-      <h2>Sidebar</h2>
+      <div className="top">
+        <MdMenu
+          className="menu"
+          size={26}
+          onClick={() => setExtended(!extended)}
+        />
+
+        <div className="new-chat">
+          <MdAdd size={22} />
+          {extended && <p>New Chat</p>}
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="bottom-item">
+          <MdHelpOutline size={22} />
+          {extended && <p>Help</p>}
+        </div>
+
+        <div className="bottom-item">
+          <MdHistory size={22} />
+          {extended && <p>Activity</p>}
+        </div>
+
+        <div className="bottom-item">
+          <MdSettings size={22} />
+          {extended && <p>Settings</p>}
+        </div>
+      </div>
     </div>
   );
 }
