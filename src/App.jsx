f525b7479a80b84310a6ca6581e9f9cfa7f4dcf4
Aditya <adityasaxena91316@gmail.com>
2026-08-03 18:16:52 +0530

    Create basic app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,12 @@
-import { useState } from "react";
-import { runChat } from "./config/gemini";
+import Main from "./components/Main/Main";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  const [prompt, setPrompt] = useState("");
-  const [response, setResponse] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const handleSend = async () => {
-    if (!prompt.trim()) return;
-
-    setLoading(true);
-
-    const result = await runChat(prompt);
-
-    setResponse(result);
-    setLoading(false);
-  };
-
   return (
-    <div style={{ padding: "30px" }}>
-      <h1>Gemini Clone</h1>
-
-      <input
-        type="text"
-        placeholder="Ask anything..."
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
-        style={{
-          width: "350px",
-          padding: "10px",
-          marginRight: "10px",
-        }}
-      />
-
-      <button onClick={handleSend}>
-        {loading ? "Loading..." : "Send"}
-      </button>
-
-      <h3>Response</h3>
-      <p>{response}</p>
-    </div>
+    <>
+      <Sidebar />
+      <Main />
+    </>
   );
 }
 
